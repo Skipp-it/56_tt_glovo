5b2e71da97d99d61a313298e9c7831bc0bcbcf9e
Mihaicv <mihaivoicu1977@gmail.com>
2021-01-23 19:53:00 +0200

    register user

--- src/main/java/com/glovo/ttglovo/favourite_meal/repository/UserRegistrationRepository.java
+++ src/main/java/com/glovo/ttglovo/favourite_meal/repository/UserRegistrationRepository.java
@@ -1,16 +1,16 @@
-package com.glovo.ttglovo.favourite_meal.repository;
-
-import com.glovo.ttglovo.favourite_meal.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-import java.util.UUID;
-
-@Repository
-public interface UserRegistrationRepository extends JpaRepository<User, UUID> {
-
-    @Query("SELECT u FROM User u WHERE u.email=?1")
-    Optional<User> findByEmail(String email);
-}
+//package com.glovo.ttglovo.favourite_meal.repository;
+//
+//import com.glovo.ttglovo.favourite_meal.model.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Optional;
+//import java.util.UUID;
+//
+//@Repository
+//public interface UserRegistrationRepository extends JpaRepository<User, UUID> {
+//
+//    @Query("SELECT u FROM User u WHERE u.email=?1")
+//    Optional<User> findByEmail(String email);
+//}
--- target/classes/glovoDB_public_meal_prices.sql
+++ target/classes/glovoDB_public_meal_prices.sql
@@ -1,3 +1,6 @@
+
+DROP TABLE IF EXISTS meal_prices;
+
 create table meal_prices
 (
     id    bigint  not null
@@ -5,6 +8,19 @@ create table meal_prices
             primary key,
     price integer not null
 );
+
+
+DROP TABLE IF EXISTS user_table;
+
+create table user_table
+(
+    id    uuid  not null
+        primary key,
+    first_name text not null,
+    last_name text not null,
+    email text not null,
+    password text not null,
+    username text not null
 
 alter table meal_prices
     owner to postgres;
@@ -267,3 +283,19 @@ INSERT INTO public.meal_prices (id, price) VALUES (52967, 69);
 INSERT INTO public.meal_prices (id, price) VALUES (52962, 4);
 INSERT INTO public.meal_prices (id, price) VALUES (52964, 70);
 INSERT INTO public.meal_prices (id, price) VALUES (52968, 15);
+
+DROP TABLE IF EXISTS user_table;
+
+create table user_table
+(
+    id    uuid  not null
+        primary key,
+    first_name text not null,
+    last_name text not null,
+    email text not null,
+    password text not null,
+    username text not null
+);
+
+alter table meal_prices
+    owner to postgres;
