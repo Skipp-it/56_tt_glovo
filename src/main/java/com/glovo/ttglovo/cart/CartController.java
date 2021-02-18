4d07f7b58956e916780fee9c3b86fce3b805cae1
Celalalt666 <mihairaducroitoru@gmail.com>
2021-02-18 15:41:43 +0200

    cart controller
    route for adding a meal to cart

--- src/main/java/com/glovo/ttglovo/cart/CartController.java
+++ src/main/java/com/glovo/ttglovo/cart/CartController.java
@@ -1,40 +1,40 @@
-//package com.glovo.ttglovo.cart;
-//
-//
-//import com.glovo.ttglovo.securityManagement.appuser.AppUserService;
-//import lombok.AllArgsConstructor;
-//import org.springframework.web.bind.annotation.*;
-//
-//import javax.validation.constraints.NotNull;
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/cart")
-//@CrossOrigin(origins = "http://localhost:3000")
-//@AllArgsConstructor
-//public class CartController {
-//
-//private final AppUserService userService;
-//
+package com.glovo.ttglovo.cart;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotNull;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/cart")
+@CrossOrigin(origins = "http://localhost:3000")
+public class CartController {
+
+    private final CartService cartService;
+
 //    @GetMapping("/{user-id}")
 //    public List<CartItem> getAllCartItems(@PathVariable("user-id") Long userId) {
 //        return userService.getAllCartItems(userId);
 //    }
-//
-//    @PostMapping()
-//    public void addNewCartProduct(@RequestBody @NotNull @PathVariable("user_id") Long userId,@PathVariable("meal_id") Long mealId) {
-//        System.out.println("--------------------intra");
-//        userService.saveCartItem(userId, mealId );
-//    }
-//
+
+    @PostMapping("/add-meal")
+    public ResponseEntity<Void> addNewCartProduct(@RequestBody @NotNull CartDao cartDao, @RequestHeader("Authorization") String token) {
+        System.out.println("--------------------intra in 26 cart controller");
+        cartService.addCartItem(cartDao, token);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
 //    @DeleteMapping("/{user_id}/{meal_id}")
 //    public void deleteCartItem( @PathVariable("user_id") Long userId, @PathVariable("meal_id") Long mealId) {
 //        userService.removeCartItem(userId, mealId );
 //    }
-//
-//    //
-////    @GetMapping("/{id}")
-////    public Product getProductById(@PathVariable("id") int id){ return favoritesService.getProductById(id);};
-//
-//
-//}
+
+    //
+//    @GetMapping("/{id}")
+//    public Product getProductById(@PathVariable("id") int id){ return favoritesService.getProductById(id);};
+
+
+}
