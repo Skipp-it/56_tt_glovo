87cbfeb7347fcdec34bd20da3957fbc071f7b4c0
stanalexandru <stanalexandrucode@gmail.com>
2021-03-03 18:11:57 +0200

    merge with favorite

--- src/main/resources/InsertData.py
+++ src/main/resources/InsertData.py
@@ -6,14 +6,18 @@ conn = psycopg2.connect("dbname=glovodb user=postgres password=root")
 cur = conn.cursor()
 
 
+def get_category(string):
+    return string.split("=")[1]
+
+
 def insert_data():
-    url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegan'
+    url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Goat'
     result = requests.get(url)
     get_json = result.json()
     elements = get_json['meals']
     for i in elements:
         cur.execute("INSERT INTO public.meal (id, price,category) VALUES (%s,%s,%s)",
-                    (i["idMeal"], random.randint(0, 100), "Vegan"))
+                    (i["idMeal"], random.randint(0, 100), get_category(url)))
 
 
 insert_data()
